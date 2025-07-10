e7c1dde3a487b6027418e20b44b2a05db3dcdfa3
Anonymous-Dust <adv050596@gmail.com>
2025-07-10 13:43:36 +0700

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -34,7 +34,7 @@ const Header = ({ onSearch, onCategoryChange, activeCategory }: HeaderProps) => 
             <div className="w-10 h-10 bg-gradient-to-r from-blue-600 to-purple-600 rounded-xl flex items-center justify-center">
               <Play className="w-5 h-5 text-white" fill="white" />
             </div>
-            <span className="text-2xl font-bold font-space text-gradient">FlixStream</span>
+            <span className="text-2xl font-bold font-space text-gradient">ZaaStream</span>
           </div>
 
           {/* Desktop Navigation */}
